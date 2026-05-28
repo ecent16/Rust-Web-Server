9816a2638f1ce6984ac408c42994d9d8d737193b
ervin.centeno <ecent416@gmail.com>
2026-05-28 13:00:02 -0400

    Added handle_connection function

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener; // Crate for tcp connections
+use std::{
+    net::{TcpListener, TcpStream}, 
+    io::{BufReader, prelude::*}, 
+};// Crate for tcp connections
 
 fn main() {
     // bind acts as a new instance of a connection
@@ -7,6 +10,20 @@ fn main() {
     for stream in listener.incoming() {
         // incoming returns a sequence of streams
         let stream = stream.unwrap(); // unwrap will handle errors
-        println!("Connection Established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(stream: TcpStream) {
+
+    let buf_reader = BufReader::new(&stream);
+
+    // Collect all the tcp stream data
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|r| r.unwrap())
+        .take_while(|l| l.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
